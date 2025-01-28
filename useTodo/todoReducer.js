ade9178d1166804a577d59467209f30d61922efb
Joel Ruitón <joelruiton@gmail.com>
2025-01-28 00:30:21 -0500

    organizar carpetas

---
+++ useTodo/todoReducer.js
@@ -0,0 +1,22 @@
+export const todoReducer = (initialState, action) => {
+  switch (action.type) {
+    case "[TODO] Add Todo":
+      return [...initialState, action.payload];
+    case "[TODO] Remove Todo":
+      return initialState.filter((todo) => todo.id !== action.payload);
+    case "[TODO] Toggle Todo":
+      return initialState.map((todo) => {
+        if (todo.id === action.payload) {
+          return {
+            ...todo,
+            done: !todo.done,
+          };
+        }
+        return todo;
+      });
+    // case "XXX":
+    //   throw new Error("Action.type == 'XXX' no esta implementada");
+    default:
+      return initialState;
+  }
+};
